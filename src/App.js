cd1062df6afadc0bb6826f1e1a7229a77b5d5f54
Erzhan Aldamatov <33819603+aldamatov@users.noreply.github.com>
2022-06-27 23:19:36 +1000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./styles.css";
+
+import React from "react";
+import Review from "./Reviews";
+function App() {
+  return (
+    <main>
+      <section className="container">
+        <div className="title">
+          <h2>Our reviews</h2>
+          <div className="underline"></div>
+        </div>
+        <Review />
+      </section>
+    </main>
+  );
+}
+
+export default App;
